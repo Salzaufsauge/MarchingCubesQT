89459438e31d18135612197c648c379cc4d5dda1
Salzaufsauge <147203308+Salzaufsauge@users.noreply.github.com>
2024-05-15 15:25:10 +0200

    Delete build directory

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MarchingCubesQT_FILE C:/dev/MarchingCubesQT/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/MarchingCubesQT.exe)
-set(__QT_DEPLOY_TARGET_MarchingCubesQT_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
